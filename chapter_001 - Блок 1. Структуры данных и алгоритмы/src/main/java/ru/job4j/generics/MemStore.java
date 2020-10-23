a65da9a8bc379e994f127ab6822c884dc5a70b20
hasover <hasover@gmail.com>
2020-10-23 15:05:55 +0300

    5.2.2. Реализовать Store<T extends Base> [#389371]

--- chapter_001 - Блок 1. Структуры данных и алгоритмы/src/main/java/ru/job4j/generics/MemStore.java
+++ chapter_001 - Блок 1. Структуры данных и алгоритмы/src/main/java/ru/job4j/generics/MemStore.java
@@ -12,35 +12,41 @@ public final class MemStore<T extends Base> implements Store<T> {
         mem.add(model);
     }
 
-    @Override
-    public boolean replace(String id, T model) {
-        for (int i = 0; i < mem.size(); i++) {
+    private int findIndex(String id) {
+        for (int i = 0; i< mem.size(); i++) {
             if (mem.get(i).getId().equals(id)) {
-                mem.set(i, model);
-                return true;
+                return i;
             }
         }
-        return false;
+        return -1;
+    }
+
+    @Override
+    public boolean replace(String id, T model) {
+        int index = findIndex(id);
+        if (index == -1) {
+            return false;
+        }
+        mem.set(index, model);
+        return true;
     }
 
     @Override
     public boolean delete(String id) {
-        for (int i = 0; i < mem.size(); i++) {
-            if (mem.get(i).getId().equals(id)) {
-                mem.remove(i);
-                return true;
-            }
+        int index = findIndex(id);
+        if (index == -1) {
+            return false;
         }
-        return false;
+        mem.remove(index);
+        return true;
     }
 
     @Override
     public T findById(String id) {
-        for (int i = 0; i < mem.size(); i++) {
-            if (mem.get(i).getId().equals(id)) {
-                return mem.get(i);
-            }
+        int index = findIndex(id);
+        if (index == -1) {
+            return null;
         }
-        return null;
+        return mem.get(index);
     }
 }
