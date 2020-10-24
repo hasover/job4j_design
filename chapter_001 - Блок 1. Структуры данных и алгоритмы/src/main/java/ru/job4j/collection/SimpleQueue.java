d76917110b035d0703c8eebece0ae44d6d0db78d
hasover <hasover@gmail.com>
2020-10-24 12:51:45 +0300

    5. Очередь на двух стеках [#389351]

--- chapter_001 - Блок 1. Структуры данных и алгоритмы/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter_001 - Блок 1. Структуры данных и алгоритмы/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -5,39 +5,20 @@ import java.util.NoSuchElementException;
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    int inSize = 0;
-    int outSize = 0;
 
     public T poll() {
-        if (inSize == 0 && outSize == 0) {
+        if (in.isEmpty() && out.isEmpty()) {
             throw new NoSuchElementException();
         }
-        int elements = inSize;
-        while (elements > 0) {
-            out.push(in.pop());
-            elements--;
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
         }
-        if (inSize == 0) {
-            outSize--;
-        } else {
-            outSize = inSize - 1;
-        }
-        inSize = 0;
         return out.pop();
     }
 
     public void push(T value) {
-        int elements = outSize;
-        while (elements > 0) {
-            in.push(out.pop());
-            elements--;
-        }
         in.push(value);
-        if (outSize == 0) {
-            inSize++;
-        } else {
-            inSize = outSize + 1;
-        }
-        outSize = 0;
     }
 }
--- chapter_001 - Блок 1. Структуры данных и алгоритмы/src/main/java/ru/job4j/collection/SimpleStack.java
+++ chapter_001 - Блок 1. Структуры данных и алгоритмы/src/main/java/ru/job4j/collection/SimpleStack.java
@@ -2,12 +2,19 @@ package ru.job4j.collection;
 
 public class SimpleStack<T> {
     private ForwardLinked<T> linked = new ForwardLinked<T>();
+    private int size;
 
     public T pop() {
+        size--;
         return linked.deleteLast();
     }
 
     public void push(T value) {
+        size++;
         linked.add(value);
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 }
