1df939deeaf7d59e61577e7d3d624535ae8af5ac
hasover <hasover@gmail.com>
2020-11-03 22:07:26 +0300

    4.0. File [#389400]

---
+++ chapter_002-Блок 2. Система ввода вывода/src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+public class Dir {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:/projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        List<File> projectFiles = new ArrayList<>();
+        projectFiles.add(file);
+        long totalSize = 0;
+        while (!projectFiles.isEmpty()) {
+            File currentFile = projectFiles.remove(projectFiles.size() - 1);
+            if (currentFile.isDirectory()) {
+                projectFiles.addAll(List.of(currentFile.listFiles()));
+            } else {
+                totalSize += currentFile.length();
+            }
+        }
+        String formattedDouble = String.format("%.2f", (double)totalSize/(1024 * 1024));
+        System.out.println("File name:" + file.getName());
+        System.out.println("File size:" + formattedDouble+ "MB");
+    }
+}
