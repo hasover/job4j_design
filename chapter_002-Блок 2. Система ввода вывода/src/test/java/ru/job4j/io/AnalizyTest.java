92301295503b45bbd4cade2eb0fb7ab5ebe8c2bc
hasover <hasover@gmail.com>
2020-11-03 19:13:50 +0300

    3.0. Тестирование IO [#389395]

--- chapter_002-Блок 2. Система ввода вывода/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002-Блок 2. Система ввода вывода/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
 
@@ -8,29 +10,30 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class AnalizyTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
-    public void whenCheckUnavailable() {
-        String target = "./data/target.txt";
-        new Analizy().unavailable("./data/server.log", target);
+    public void whenCheckUnavailable() throws IOException {
+        String source = "./data/server.log";
+        File target = folder.newFile("target.txt");
+        new Analizy().unavailable("./data/server.log", target.getAbsolutePath());
         try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
-            assertThat(reader.readLine(), is ("10:58:01;10:59:01"));
-            assertThat(reader.readLine(), is ("11:01:02;11:02:02"));
-            assertThat(reader.readLine(), is ("11:05:34;11:09:00"));
-        } catch (IOException e) {
-            e.printStackTrace();
+            assertThat(reader.readLine(), is("10:58:01;10:59:01"));
+            assertThat(reader.readLine(), is("11:01:02;11:02:02"));
+            assertThat(reader.readLine(), is("11:05:34;11:09:00"));
         }
     }
 
     @Test
-    public void whenCheckUnavailable2() {
-        String target = "./data/target2.txt";
-        new Analizy().unavailable("./data/server2.log", target);
+    public void whenCheckUnavailable2() throws IOException {
+        String source = "./data/server.log";
+        File target = folder.newFile("target.txt");
+        new Analizy().unavailable("./data/server2.log", target.getAbsolutePath());
         try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
             assertThat(reader.readLine(), is ("10:58:01;10:59:01"));
             assertThat(reader.readLine(), is ("11:01:02;"));
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-
 }
