e0bae2ecfd28720146244b79ba71e3d961680bcd
hasover <hasover@gmail.com>
2020-11-04 22:44:32 +0300

    5. Валидация параметров запуска. [#389396]

--- chapter_002-Блок 2. Система ввода вывода/src/main/java/ru/job4j/io/Search.java
+++ chapter_002-Блок 2. Система ввода вывода/src/main/java/ru/job4j/io/Search.java
@@ -9,9 +9,11 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("C:/projects");
-        search(start, p-> p.toFile().getName().endsWith(".java")).forEach(System.out::println);
-        search(start, p-> p.toFile().length() > 1000L).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("2 parameters required : folder name, file types");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p-> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> predicate) throws IOException {
