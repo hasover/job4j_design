a32e464dc029e5e155bae341dce7953db18758a1
hasover <hasover@gmail.com>
2020-11-04 23:29:41 +0300

    5.1. Именованные аргументы [#389401]

---
+++ chapter_002-Блок 2. Система ввода вывода/src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,38 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        String value = values.get(key);
+        if (value == null) {
+            throw new IllegalArgumentException();
+        }
+        return value;
+    }
+
+    private void parse(String[] args) {
+        for (String arg : args) {
+            String[] params = arg.split("=");
+            values.put(params[0].substring(1), params[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
