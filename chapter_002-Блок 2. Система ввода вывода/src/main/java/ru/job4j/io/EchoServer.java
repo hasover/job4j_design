59505840a5ea6844fa3ee22a5b1549b34ddb3c88
hasover <hasover@gmail.com>
2020-11-08 23:58:19 +0300

    1. Бот [#389420]

--- chapter_002-Блок 2. Система ввода вывода/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002-Блок 2. Система ввода вывода/src/main/java/ru/job4j/io/EchoServer.java
@@ -14,18 +14,22 @@ public class EchoServer {
             Socket socket = server.accept();
             try (OutputStream out = socket.getOutputStream();
                  BufferedReader in = new BufferedReader(
-                                     new InputStreamReader(socket.getInputStream()))) {
+                         new InputStreamReader(socket.getInputStream()))) {
                 String str;
                 String message = "";
                 while (!(str = in.readLine()).isEmpty()) {
                     if (str.contains("/?msg=")) {
-                       message = str.split(" ")[1].split("=")[1];
+                        message = str.split(" ")[1].split("=")[1];
                     }
                     System.out.println(str);
                 }
-                out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
-                if (message.equals("Bye")) {
+                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                if (message.equals("Hello")) {
+                    out.write("Hello, dear friend.".getBytes());
+                } else if (message.equals("Exit")) {
                     break;
+                } else {
+                    out.write(message.getBytes());
                 }
             }
         }
