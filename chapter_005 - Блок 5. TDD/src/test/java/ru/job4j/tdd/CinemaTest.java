0d55f44adf50b47783b0d9c0204c938ed8c06796
hasover <hasover@gmail.com>
2021-02-19 22:43:12 +0300

    2. Что такое TDD? [#389425]

---
+++ chapter_005 - Блок 5. TDD/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.tdd;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class CinemaTest {
+
+    @Test
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Test
+    public void add() {
+        Cinema cinema = new Cinema3D();
+        Session newSession = new Session3D();
+        cinema.add(newSession);
+        assertThat(cinema.find(session -> session.equals(newSession)).isEmpty(), is(false));
+    }
+
+
+}
